2764f123a803ab9d8ee0951549e96d60332b973f
Egor Shmelev <shmelevyegor25@gmail.com>
2024-06-20 17:16:47 -0300

    task 3

--- materials/models.py
+++ materials/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+from users.models import NULLABLE
+
 
 class Course(models.Model):
     title = models.CharField(max_length=100, verbose_name='название')
-    image = models.ImageField(upload_to='courses', verbose_name='картинка')
+    image = models.ImageField(upload_to='courses', verbose_name='картинка', **NULLABLE)
     description = models.TextField(verbose_name='описание')
 
     def __str__(self):
@@ -17,7 +19,7 @@ class Course(models.Model):
 class Lesson(models.Model):
     title = models.CharField(max_length=100, verbose_name='название')
     description = models.TextField(verbose_name='описание')
-    image = models.ImageField(upload_to='lessons', verbose_name='картинка')
+    image = models.ImageField(upload_to='lessons', verbose_name='картинка', **NULLABLE)
     url = models.URLField(verbose_name='ссылка на видео')
 
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
