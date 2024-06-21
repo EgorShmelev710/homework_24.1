0aee525f47e019997a2bfb3a3b2c708b482b97eb
Egor Shmelev <shmelevyegor25@gmail.com>
2024-06-20 21:23:38 -0300

    additional task

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from users.apps import UsersConfig
+from rest_framework.routers import DefaultRouter
+
+from users.views import UserViewSet
+
+app_name = UsersConfig.name
+
+router = DefaultRouter()
+router.register('', UserViewSet, basename='users')
+
+urlpatterns = [
+
+] + router.urls
