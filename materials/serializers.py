31c92275f7dce21a0ba99347b20f25d90c349088
Egor Shmelev <shmelevyegor25@gmail.com>
2024-06-24 16:08:10 -0300

    homework_24.2 task1

--- materials/serializers.py
+++ materials/serializers.py
@@ -4,6 +4,12 @@ from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_lessons_count(instance):
+        return instance.lesson_set.count()
+
     class Meta:
         model = Course
         fields = '__all__'
