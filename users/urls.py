e051bf634bfd9738efa2d5e6d529906b9e7569bb
Egor Shmelev <shmelevyegor25@gmail.com>
2024-06-27 22:52:48 -0300

    homework_24.2 task4

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
+from django.urls import path
+
 from users.apps import UsersConfig
 from rest_framework.routers import DefaultRouter
 
-from users.views import UserViewSet
+from users.views import UserViewSet, PaymentsListAPIView
 
 app_name = UsersConfig.name
 
@@ -9,5 +11,5 @@ router = DefaultRouter()
 router.register('', UserViewSet, basename='users')
 
 urlpatterns = [
-
+    path('payments/', PaymentsListAPIView.as_view(), name='payments'),
 ] + router.urls
