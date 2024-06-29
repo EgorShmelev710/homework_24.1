f35a656d7eb206bc64d98961c80bf3d5600dfd80
Egor Shmelev <shmelevyegor25@gmail.com>
2024-06-29 15:08:12 -0300

    homework_25.1 task1

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from users.apps import UsersConfig
 from rest_framework.routers import DefaultRouter
@@ -11,5 +16,7 @@ router = DefaultRouter()
 router.register('', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('payments/', PaymentsListAPIView.as_view(), name='payments'),
+      path('payments/', PaymentsListAPIView.as_view(), name='payments'),
+      path('token/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='token_obtain_pair'),
+      path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 ] + router.urls
