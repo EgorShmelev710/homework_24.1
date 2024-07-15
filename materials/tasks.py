0f14e1b42551b7b4fab448147ea9d1586d75dda2
Egor Shmelev <shmelevyegor25@gmail.com>
2024-07-14 22:53:06 -0300

    homework_26.2 task3

--- materials/tasks.py
+++ materials/tasks.py
@@ -1,6 +1,9 @@
+from datetime import datetime, timedelta
+
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
+from django.utils import timezone
 
 from materials.models import Course, Subscription
 from users.models import User
@@ -19,3 +22,15 @@ def update_notification(course_pk):
                 from_email=settings.EMAIL_HOST_USER,
                 recipient_list=[user.email],
             )
+
+
+@shared_task
+def check_last_login():
+    users = User.objects.filter(last_login__isnull=False)
+    for user in users:
+        if timezone.now() - user.last_login > timedelta(days=30):
+            user.is_active = False
+            user.save()
+            print(f'Пользователь {user.email} отключен')
+        else:
+            print(f'Пользователь {user.email} в порядке')
